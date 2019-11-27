68903289e08ee2755f2539d8826111001481a9ff
sleekcharly <c_ukasoanya@yahoo.com>
2019-11-27 03:23:04 +0100

    second commit

--- public/js/app.js
+++ public/js/app.js
@@ -20,22 +20,33 @@ class ProductList extends React.Component {
 class Product extends React.Component {
     render() {
         return (
-            <div className="item">
+            <div className='item'>
                 <div className='image'>
-                    <img src='images/products/image-aqua.png' />
+                    <img src={this.props.productImageUrl} />
                 </div>
 
                 <div className='middle aligned content'>
-                    <div className='description'>
-                        <a>Fort knight</a>
-                        <p>Authentic renaissance actors, delivered in just two weeks.</p>
+                    <div className='header'>
+                        <a>
+                            <i className='large caret up icon' />
+                        </a>
+                        {this.props.votes}
                     </div>
-                    
+
+                    <div className='description'>
+                        <a href={this.props.url}>
+                            {this.props.title}
+                        </a>
+                        <p>
+                            {this.props.description}
+                        </p>
+                    </div>
+
                     <div className='extra'>
-                        <span>Submitted by: </span>
-                        <img    
+                        <span>Submitted by:</span>
+                        <img
                             className='ui avatar image'
-                            src='images/avatars/daniel.jpg'
+                            src={this.props.submitterAvatarUrl}
                         />
                     </div>
                 </div>
