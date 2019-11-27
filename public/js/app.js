a47fee289cf3a3f368d7207427632a55e6c84100
sleekcharly <c_ukasoanya@yahoo.com>
2019-11-27 16:27:34 +0100

    implementation of ability to upvote

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,36 @@
 class ProductList extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            products: [],
+        };
+
+        this.handleProductUpVote = this.handleProductUpVote.bind(this);
+    }
+
+    handleProductUpVote(productId) {
+        const nextProducts = this.state.products.map((product) => {
+            if (product.id === productId) {
+                return Object.assign({}, product, {
+                    votes: product.votes + 1,
+                });
+            } else {
+                return product;
+            }
+        });
+        this.setState({
+            products: nextProducts,
+        });
+    }
+
+    componentDidMount() {
+        this.setState({ products: Seed.products });
+    }
+
     render() {
-        const products = Seed.products.sort((a, b) => (
+        const products = this.state.products.sort((a, b) => (
             b.votes - a.votes
         ));
         const productComponents = products.map((product) => (
@@ -13,6 +43,7 @@ class ProductList extends React.Component {
                 votes={product.votes}
                 submitterAvatarUrl={product.submitterAvatarUrl}
                 productImageUrl={product.productImageUrl}
+                onVote={this.handleProductUpVote}
             />
         ));
         return (
@@ -24,6 +55,17 @@ class ProductList extends React.Component {
 }
 
 class Product extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.handleUpVote = this.handleUpVote.bind(this);
+    }
+
+    handleUpVote() {
+        this.props.onVote(this.props.id);
+    }
+
     render() {
         return (
             <div className='item'>
@@ -33,7 +75,7 @@ class Product extends React.Component {
 
                 <div className='middle aligned content'>
                     <div className='header'>
-                        <a>
+                        <a onClick={this.handleUpVote}>
                             <i className='large caret up icon' />
                         </a>
                         {this.props.votes}
